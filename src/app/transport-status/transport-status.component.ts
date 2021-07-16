dcf658c363ef575bff14b287d62e664021786df3
GDHohil <korec.illia@gmail.com>
2021-07-16 16:41:17 +0300

    initial setup

---
+++ src/app/transport-status/transport-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transport-status',
+  templateUrl: './transport-status.component.html',
+  styleUrls: ['./transport-status.component.scss']
+})
+export class TransportStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
