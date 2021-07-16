b62dd6410e0111b5a7e69f32bae07bd79f5a8b9b
GDHohil <korec.illia@gmail.com>
2021-07-16 17:03:53 +0300

    added navigation to transport status

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransportStatusComponent } from './transport-status/transport-status.component';
 
 const routes: Routes = [
   {
     path: 'main',
     component: MainComponent
+  },
+  {
+    path: 'transport-status',
+    component: TransportStatusComponent
   }
 ];
 
